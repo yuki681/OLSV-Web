935799fc9f43317342b2a291b3b17a36c20ebbe6
Yuki Yamasaki <yamasaki.yuki.s5@gmail.com>
2026-02-15 13:43:47 +0900

    Add Search Function

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,9 +1,22 @@
 class LicensesController < ApplicationController
   def index
-    @q = params[:q]
+    @q = params[:q].to_s
+    q_tokens = @q.split(/[[:blank:]]+/).reject(&:blank?)
 
-    # TODO: 検索機能の追加
-    @pagy, @licenses = pagy(:offset, License.all.order(:name), limit: 20)
+    if q_tokens.present?
+      # TODO: PostgreSQLの場合はILIKEを使用する必要がある。
+      # レコード数700件程度であれば、十分高速（10ms未満）であることを確認した。
+      @licenses = License.all
+      q_tokens.each do |token|
+        pattern = "%#{ActiveRecord::Base.sanitize_sql_like(token)}%"
+        @licenses = @licenses.where("name LIKE ?", pattern)
+      end
+
+    else
+      @licenses = License.none
+    end
+
+    @pagy, @licenses = pagy(:offset, @licenses, limit: 20)
   end
 
   def show
